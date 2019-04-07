b7d7a7f89a7537b65a491ccac0b5b8c608bab445
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-07 00:06:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,7 +9,8 @@ class Song
   def self.new_by_filename(filename)
     song = self.new
     song.title = filename.split(" - ")[1]
-    song
+    song = self.new(song_name)
+    artist = Artist.find_or_create_by_name(artist_name)
+    artist.add_song(song)
   end
-    
-end 
+end
