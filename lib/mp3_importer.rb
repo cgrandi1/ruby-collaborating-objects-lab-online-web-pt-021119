5385f2374c53de01ad4d0f6f1a27da42f4ee8b8c
Carlton Dwayne <carltondgrandison@gmail.com>
2019-04-07 00:03:23 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,18 +1,12 @@
-
 class MP3Importer
-
-  attr_accessor :path
-
+  
+  attr_accessor :path 
+  
   def initialize(path)
     @path = path
-  end
-
-  def files
-    directory = Dir.entries(@path+"/")
-    directory.select {|file_path| file_path.end_with? ".mp3" }
-  end
-
-  def import
-    self.files.each {|| Song.new_by_filename(files)}
-  end
-end
+  end 
+  
+  def files 
+    Dir.glo('.').each {|file| puts file }
+  end 
+end 
